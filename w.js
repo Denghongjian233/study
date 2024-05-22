fd86947891972db0cab8fc6c883be1f42f7ffd0a
邓宏健 <2391240816@qq.com>
2024-05-22 21:47:47 +0800

    feat: 插件 和画图

---
+++ w.js
@@ -0,0 +1,19 @@
+/**
+ * @description: 
+ * @param {*} a
+ * @param {*} b
+ * @return {*}
+ */
+function add(a, b) {
+    return a + b
+}
+
+/**
+ * @description: 
+ * @param {*} a
+ * @param {*} b
+ * @return {*}》
+ */
+function dec(a, b) {
+    return a - b
+}
